9eb77854b057000d11d407d0b2442ba21a4c76dd
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-12-05 16:52:15 +0100

    cominciato modulo header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function () {
+    return (
+        <> 
+            <header className="wrapper">
+                <img src="/img/dc-logo.png" alt="dc logo" id="logo"/>
+                <nav>
+                    <ul>
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Header from "./components/Header";
+import "./components/Header.module.css";
 
 createRoot(document.getElementById("root")).render(
     <>
-        <App />
+        <Header />
     </>
 );
