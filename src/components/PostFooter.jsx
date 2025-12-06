d46100fb551b49889c8ca063e0e22d0fd3a6e9ef
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-12-06 16:55:40 +0100

    completato layout generale

--- src/components/PostFooter.jsx
+++ src/components/PostFooter.jsx
@@ -1,9 +1,21 @@
-import styles from "./PostFooter.component.css";
+import styles from "./PostFooter.module.css";
 
-export default function() {
+export default function () {
     return (
         <>
-            <div className={styles.postFooter}></div>
+            <div className={styles.postFooter}>
+                <div className={`${styles.flex} wrapper`}>
+                    <button className={styles.button}>SIGN-UP NOW</button>
+                    <div className={`${styles.flex} ${styles.socials}`}>
+                        <h1 className={styles.accent}>FOLLOW US</h1>
+                        <img src="public/img/footer-facebook.png" alt="" />
+                        <img src="public/img/footer-periscope.png" alt="" />
+                        <img src="public/img/footer-pinterest.png" alt="" />
+                        <img src="public/img/footer-twitter.png" alt="" />
+                        <img src="public/img/footer-youtube.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import PostFooter from "./components/PostFooter";
 
 createRoot(document.getElementById("root")).render(
     <>
         <Header />
         <Main />
         <Footer />
+        <PostFooter />
     </>
 );
