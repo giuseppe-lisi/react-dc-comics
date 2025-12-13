713947587543e34caadcb62d178b5c6b38e964f9
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-12-13 10:58:25 +0100

    compleato header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,22 @@ import { navLinks } from "../data/navLinks";
 export default function () {
     return (
         <>
-            <header className="wrapper">
-                <div className={s.header}>
+            <header>
+                <div className={`${s.header} wrapper`}>
                     <img src="/img/dc-logo.png" alt="dc logo" />
                     <nav className={s.nav}>
                         <ul className={s.ul}>
                             {navLinks.map((link) => {
-                                return <li key={link.key} className={link.isActive ? `${s.selected}` : ""}>{link.label}</li>;
+                                return <li 
+                                            key={link.key} 
+                                            className={link.isActive ? `${s.selected}` : ""}>
+                                            {link.label}
+                                        </li>;
                             })}
                         </ul>
                     </nav>
                 </div>
+                <div className={s.headerImg}></div>
             </header>
         </>
     );
